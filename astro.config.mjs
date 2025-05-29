86e784c66e54fcd98784c55b08e6fa5f6a78e8a6
mimcqueen <mcqueenmitch66@gmail.com>
2025-05-29 18:50:50 -0500

    tailwind setup correctly, attempt to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
+
 export default defineConfig({
-      site: 'https://mimcqueen.github.io'
-});
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: 'https://mcqueensolutions.com',
+  compressHTML: true,
+})
